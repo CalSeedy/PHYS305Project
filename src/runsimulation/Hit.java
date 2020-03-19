e33f4c12dba63d5f8426b265139b9fba08cffaf6
Callum Seed <callumseed@hotmail.co.uk>
2020-03-19 20:41:22 +0000

    Merged RealTime simulation from master and added collision detection and near misses

--- src/runsimulation/Hit.java
+++ src/runsimulation/Hit.java
@@ -6,17 +6,164 @@ class Hit {
 
     int [] hits;   //will store the number of hits on each object
     String[] planetNames;
+    int[] planetIndexes;
     
-    public Hit(String[] names){
+    public Hit(SolarSystem sys){
+        Body[] objs = sys.getObjects();
+        int c = 0;
+        for (Body b : objs){
+            if (b.isAsteroid){
+                c++;
+            }
+        }
+        
+        String[] names = new String[objs.length - c];
+        int[] idxs = new int[objs.length - c];
         //maybe create an aray of planet names
         hits = new int [names.length]; //objects.length
         planetNames = names;
+        planetIndexes = idxs;
+        
+        index(sys);
         
     }
     
-    public void checkHit(){
-        
+    private void index(SolarSystem sys){
+        Body[] objs = sys.getObjects();
+        String[] names = new String[planetNames.length];
+        int[] idxs = new int[planetIndexes.length];
+        int k = 0;
+        for (Body b : objs){
+            if (!b.isAsteroid){
+                names[k] = b.name;
+                idxs[k] = sys.findObjectIndex(b.name);
+            }
+        }
     }
+    
+    private int getIndex(Body[] arr, String identifier){
+        // create a counter to keep track of current position in objects
+        int c = 0;
+        
+        // while we havent reached the end of the array of Bodies
+        while (c < arr.length){
+            // for each body, b, in objects (one at a time)
+            for (Body b : arr){
+                
+                // check if the Body has the same name as the inputted "identifier"
+                if (b.name.equals(identifier)){
+                    // if it does, return the counter as the index of the Body
+                    return c;
+                } else {
+                    // otherwise, increment the counter by 1
+                    c++;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    
+    public void checkHit(SolarSystem sys){
+        Body[] objects = sys.getObjects();
+        
+        for (int j = 0; j < objects.length; j++){
+            for (int i = j; i < objects.length; i++){
+                Body b1 = objects[i];
+                Body b2 = objects[j];
+                if (i != j){
+                    double d = b1.distanceToBody(b2);
+                    double radii = b1.getRadius() + b2.getRadius();
+
+                    if (d <= 1.25*radii){
+                        //System.out.println(String.format("%e: %s collided with %s", sys.time, b1.name, b2.name));
+                        if (b1.isAsteroid && !b2.isAsteroid){
+                            System.out.println(String.format("\t %s was removed!", b1.name));
+                            sys.removeObject(sys.findObjectIndex(b1.name));
+                            
+                        } else if (!b1.isAsteroid && b2.isAsteroid){
+                            System.out.println(String.format("\t %s was removed!", b2.name));
+                            sys.removeObject(sys.findObjectIndex(b2.name));
+                        } else {
+                            // do stuff for planet-planet collisions
+                        }
+                    } else if (d <= 1.5*radii){
+                        if (b1.isAsteroid){
+                            System.out.println(String.format("%e: %s barely passed by %s", sys.time, b1.name, b2.name));
+                        } else {
+                            System.out.println(String.format("%e: %s barely passed by %s", sys.time, b2.name, b1.name));
+                        }
+                    }
+                }
+            }       
+        }
+    }
+    
+    /*
+    public void Hit(){
+        
+        
+        for (int j = 0; j < objects.length; j++){
+            for (int i = 0; i < objects.length; i++){
+                double d = objects[i].distanceToBody(objects[j]);
+                double radii = objects[i].getRadius() + objects[j].getRadius();
+                if ( d <= radii){
+                    
+                    double m1 = objects[i].getMass();
+                    double m2 = objects[j].getMass();
+                    
+                    if (m1 / m2 > Math.pow(10,6)) { //if m2 is much smaller than m1
+                        //m2 will merge with m1 and transfer its momentum to m1
+
+                        double[] momentum1 = objects[i].getMomentum();
+                        double[] momentum2 = objects[j].getMomentum();
+                        double[] new_momentum =  {0.,0.,0.};
+                        new_momentum[0] = momentum1[0]+momentum2[0];
+                        new_momentum[1] = momentum1[1]+momentum2[1];
+                        new_momentum[2] = momentum1[2]+momentum2[2];
+                    
+                        double new_mass = objects[i].getMass()+objects[j].getMass();
+                    
+                        String name1 = objects[j].getName(); //the names of the objects which have collided
+                        String name2 = objects[i].getName();
+                        
+                        objects[j].updateMass(objects[i].getMass());
+                        objects[j].updateVelocity(new_momentum);
+                        
+                        //Scalett is working on the below
+                        ///*if (name1.indexOf("Asteroid") == -1){ //if the string "Asteroid" is not in the name
+                            //if name1 is in the list 'names':
+                            if (names.contains(name1)){ //google isn't helping me!!! I want to see whether this list contains name1
+                                //find the index
+                                //add 1 to the same index in the hit list
+                            }
+                            //else:
+                                //add the name to the list 'names'
+                                
+                                //find the index of that name
+                                //add 1 to the same index in the hit list
+                                //OR add the same to the end of the 'names' list and add 1 to the end of the 'hits' list
+                                names.add(name1); //add the name to the end of the 'names' list
+                                hits.add(1); //add one to the end of the 'hits' list
+                        }
+                        */
+                        /*
+                        removeObject(i);
+
+                        //hits[j] ++;
+                        //hits[i] ++;
+                    
+                    //update the mass and momentum x of the first body to include the mass of the second
+                    //update the speed of the first body
+                    //delete the seocnd body
+                    //record what body hit what
+                    //or should I just delete the body with 'asteroid' in its name??
+                    }
+                }
+            }
+        }
+    }
+    */
 }            
     
 
--- src/runsimulation/SolarSystem.java
+++ src/runsimulation/SolarSystem.java
@@ -1,7 +1,6 @@
-
 package runsimulation;
 
-import java.util.*;
+import java.util.Random;
 
 // SolarSystem class manages all the objects that are inside the system,
 // handles the addition/removal of any objects and has the responsibility
@@ -12,6 +11,8 @@ public class SolarSystem {
     
     private Body[] objects;         // store a array of all bodies in the system
     private int astNum = 0;         // store number of asteroids in the system
+    public double time = 0.; 
+    public Hit Hits;
     
     // blank constructor to initialise the system with a star ("Sun")
     // Sun has the same properties (im SI) as the Sun, from Google
@@ -29,6 +30,9 @@ public class SolarSystem {
         
         // overwrite the null array
         objects = new_objects;
+        
+        Hit hit = new Hit(this);
+        Hits = hit;
     }
     
     
@@ -51,6 +55,9 @@ public class SolarSystem {
         
         // overwrite the stored array
         objects = new_objects;
+        
+        Hit hit = new Hit(this);
+        Hits = hit;
     }
     
     
@@ -79,7 +86,10 @@ public class SolarSystem {
             }
         }
         // overwrite the stored array of Bodies
-        objects = new_objects;      
+        objects = new_objects; 
+        
+        Hit hit = new Hit(this);
+        Hits = hit;
     }
     
     // getter for the array of objects
@@ -175,127 +185,70 @@ public class SolarSystem {
         double[][] v2 = new double[objects.length][3]; //array of final velocities      
 
         //loops over every body and every other body:
-            for (int j = 0; j < objects.length; j++){ //j will be m1
-                for (int i = 0; i < objects.length; i++){ //i will be m2
-                    if (i != j){
-                        double[] positionVec = objects[i].vectorToBody(objects[j]); //the vector between the bodies (from m2 to m1)
-                        double d = objects[i].distanceToBody(objects[j]); //distance betweem m2 and m1
-                        double factor = G*objects[i].getMass()/ (Math.pow(d, 3)); //the acceleration factor
-                        accelerations[j][0] += factor * positionVec[0]; //the acceleration on m1 due to m2 in the x-direction is added on in every loop over j
-                        accelerations[j][1] += factor * positionVec[1];
-                        accelerations[j][2] += factor * positionVec[2];
-                        
-                        v1[j] = objects[j].getVelocity(); //saving the initial velocity of m1
-                        position1[j] = objects[j].getPosition(); //saving the initial position of m1
-                    }
-                }                
-            }
-           
-    for (int i = 0; i < accelerations.length; i++){
-        v2[i][0] = v1[i][0] + accelerations[i][0]*timestep; //the final acceleration of m1 in the x-direction
-        v2[i][1] = v1[i][1] + accelerations[i][1]*timestep;
-        v2[i][2] = v1[i][2] + accelerations[i][2]*timestep;  
-        
-        positions2[i][0] = position1[i][0] + v2[i][0]*timestep; //the final velocity of m1 in the x-direction
-        positions2[i][1] = position1[i][1] + v2[i][1]*timestep;                        
-        positions2[i][2] = position1[i][2] + v2[i][2]*timestep;
-        
-        objects[i].setPosition(positions2[i][0], positions2[i][1], positions2[i][2]); //put the final position into the array 
-        objects[i].setVelocity(v2[i][0], v2[i][1], v2[i][2]); //put the final velocity into the array
-        }
-    }    
-      
-    
-    public void Hit(){
-        int [] hits;  //will store the number of hits on each object
-        String[] names;
-        
-        for (int j = 0; j < objects.length; j++){
-            for (int i = 0; i < objects.length; i++){
-                double d = objects[i].distanceToBody(objects[j]);
-                double radii = objects[i].getRadius() + objects[j].getRadius();
-                if ( d <= radii){
-                    
-                    double m1 = objects[i].getMass();
-                    double m2 = objects[j].getMass();
-                    
-                    if (m1 / m2 > Math.pow(10,6)) { //if m2 is much smaller than m1
-                        //m2 will merge with m1 and transfer its momentum to m1
+        for (int j = 0; j < objects.length; j++){ //j will be m1
+            for (int i = 0; i < objects.length; i++){ //i will be m2
+                if (i != j){
+                    double[] positionVec = objects[i].vectorToBody(objects[j]); //the vector between the bodies (from m2 to m1)
+                    double d = objects[i].distanceToBody(objects[j]); //distance betweem m2 and m1
+                    double factor = G*objects[i].getMass()/ (Math.pow(d, 3)); //the acceleration factor
+                    accelerations[j][0] += factor * positionVec[0]; //the acceleration on m1 due to m2 in the x-direction is added on in every loop over j
+                    accelerations[j][1] += factor * positionVec[1];
+                    accelerations[j][2] += factor * positionVec[2];
 
-                        double[] momentum1 = objects[i].getMomentum();
-                        double[] momentum2 = objects[j].getMomentum();
-                        double[] new_momentum =  {0.,0.,0.};
-                        new_momentum[0] = momentum1[0]+momentum2[0];
-                        new_momentum[1] = momentum1[1]+momentum2[1];
-                        new_momentum[2] = momentum1[2]+momentum2[2];
-                    
-                        double new_mass = objects[i].getMass()+objects[j].getMass();
-                    
-                        String name1 = objects[j].getName(); //the names of the objects which have collided
-                        String name2 = objects[i].getName();
-                        
-                        objects[j].updateMass(objects[i].getMass());
-                        objects[j].updateVelocity(new_momentum);
-                        
-                        //Scalett is working on the below
-                        /*if (name1.indexOf("Asteroid") == -1){ //if the string "Asteroid" is not in the name
-                            //if name1 is in the list 'names':
-                            if (names.contains(name1)){ //google isn't helping me!!! I want to see whether this list contains name1
-                                //find the index
-                                //add 1 to the same index in the hit list
-                            }
-                            //else:
-                                //add the name to the list 'names'
-                                
-                                //find the index of that name
-                                //add 1 to the same index in the hit list
-                                //OR add the same to the end of the 'names' list and add 1 to the end of the 'hits' list
-                                names.add(name1); //add the name to the end of the 'names' list
-                                hits.add(1); //add one to the end of the 'hits' list
-                        }
-                        */
-                        removeObject(i);
-
-                        //hits[j] ++;
-                        //hits[i] ++;
-                    
-                    //update the mass and momentum x of the first body to include the mass of the second
-                    //update the speed of the first body
-                    //delete the seocnd body
-                    //record what body hit what
-                    //or should I just delete the body with 'asteroid' in its name??
-                    }
+                    v1[j] = objects[j].getVelocity(); //saving the initial velocity of m1
+                    position1[j] = objects[j].getPosition(); //saving the initial position of m1
                 }
-            }
+            }                
         }
+           
+        for (int i = 0; i < accelerations.length; i++){
+            v2[i][0] = v1[i][0] + accelerations[i][0]*timestep; //the final acceleration of m1 in the x-direction
+            v2[i][1] = v1[i][1] + accelerations[i][1]*timestep;
+            v2[i][2] = v1[i][2] + accelerations[i][2]*timestep;  
+
+            positions2[i][0] = position1[i][0] + v2[i][0]*timestep; //the final velocity of m1 in the x-direction
+            positions2[i][1] = position1[i][1] + v2[i][1]*timestep;                        
+            positions2[i][2] = position1[i][2] + v2[i][2]*timestep;
+
+            objects[i].setPosition(positions2[i][0], positions2[i][1], positions2[i][2]); //put the final position into the array 
+            objects[i].setVelocity(v2[i][0], v2[i][1], v2[i][2]); //put the final velocity into the array
+        }
+    
+        time += timestep;
     }
     
     private double[] fvector(double[][] fullstate){
         
-        double[][] accelerations = new double[objects.length][3]; //inituialising an array of accelerations for the bodies
+        double[][] accelerations = new double[objects.length][3]; //initialising an array of accelerations for the bodies
         
         double[] fvec = new double[objects.length*8];
         for (int i = 0; i < objects.length; i++){
             fvec[i*8 + 0] = fullstate[i][3];
             fvec[i*8 + 1] = fullstate[i][4];
             fvec[i*8 + 2] = fullstate[i][5];
+            
             for (int j = 0; j < objects.length; j++){
-                if (i != j){
-                    //double m1 = fullstate[i][6];
-                    double m2 = fullstate[j][6];
-                    
-                    double[] r21 = {fullstate[i][0] - fullstate[j][0], fullstate[i][1] - fullstate[j][1], fullstate[i][2] - fullstate[j][2]};
-                    double d = Math.sqrt(r21[0]*r21[0] + r21[1]*r21[1] + r21[2]*r21[2]);
-        
-                    double factor = -G*m2 / (d*d*d);
-                    
-                    // a12 = -G*m2 / (|r21|)^3 * r21
-                    
-                    accelerations[i][0] += factor * r21[0]; //the acceleration on m1 due to m2 in the x-direction is added on in every loop over j
-                    accelerations[i][1] += factor * r21[1];
-                    accelerations[i][2] += factor * r21[2];
-                    
-                    
+                if (i != j) {
+                    if ((objects[i].isAsteroid) && (objects[j].isAsteroid)){
+                        accelerations[i][0] += 0.; //the acceleration on m1 due to m2 in the x-direction is added on in every loop over j
+                        accelerations[i][1] += 0.;
+                        accelerations[i][2] += 0.;
+                    } else {
+                        //double m1 = fullstate[i][6];
+                        double m2 = fullstate[j][6];
+
+                        double[] r21 = {fullstate[i][0] - fullstate[j][0], fullstate[i][1] - fullstate[j][1], fullstate[i][2] - fullstate[j][2]};
+                        double d = Math.sqrt(r21[0]*r21[0] + r21[1]*r21[1] + r21[2]*r21[2]);
+
+                        double factor = -G*m2 / (d*d*d);
+
+                        // a12 = -G*m2 / (|r21|)^3 * r21
+
+                        accelerations[i][0] += factor * r21[0]; //the acceleration on m1 due to m2 in the x-direction is added on in every loop over j
+                        accelerations[i][1] += factor * r21[1];
+                        accelerations[i][2] += factor * r21[2];
+
+                    }
                 }
             }
         }
@@ -410,7 +363,7 @@ public class SolarSystem {
         }
         
         
-        
+        time += timestep;
     }
     
     // method that creates a new instance of an asteroid (which is just a Body)
@@ -422,7 +375,7 @@ public class SolarSystem {
         for (Body b : objects){
             double[] pos = b.getPosition();
             double distance = magnitude(pos);
-            if ((distance > furthest) && !(b.name.contains("Asteroid"))){
+            if ((distance > furthest) && !(b.isAsteroid)){
                 furthest = distance;
             }
         }
@@ -469,7 +422,7 @@ public class SolarSystem {
         
         // use Density * Volume to calculate mass; assume Density of asteroids = 5000 kg/m^3
         // Volume of a sphere = 4/3 * PI * r^3
-        double a_mass = 5000. * (4./3.) * Math.PI * a_radius * a_radius * a_radius;
+        double a_mass = 5000 * (4./3.) * Math.PI * a_radius * a_radius * a_radius;
         
         // create the asteroid with the above calculated values with the name AsteroidX, where X
         // is the current number of asteroids in the system
@@ -479,5 +432,65 @@ public class SolarSystem {
         addObject(ast);
         astNum++; 
     }
+    
+    // method that creates a new instance of an asteroid (which is just a Body)
+    // the asteroid is generated with a random position on a unit sphere and always
+    // has a velocity towards the Earth
+    public void generateAsteroidLine(double x_in, double y_in, double z_in, int thickness, int N, boolean target){
+        /*
+        // Find the object that is the furthest away (that isnt an asteroid)
+        double furthest = 0.;
+        for (Body b : objects){
+            double[] pos = b.getPosition();
+            double distance = magnitude(pos);
+            if ((distance > furthest) && !(b.name.contains("Asteroid"))){
+                furthest = distance;
+            }
+        }
+        */
+        Random rand = new Random();
+        for (int i = 0; i < N; i++){
+            double x, y, z;
+            x = x_in - rand.nextDouble()*(thickness*148.28e7);
+            y = y_in - rand.nextDouble()*(2.*7.78574E11);
+            z = z_in; // can do same as above but with some "width/height"
+            
+            double[] a_pos = {x, y, z};
+            double[] a_vel = {0., 0., 0.};
+            double vx, vy, vz;
+
+            if (target){
+                // Find out where the Earth is
+                int ind = findObjectIndex("Earth");
+                Body E = getObject(ind);
+                double[] E_pos = E.getPosition();
+
+                // create a velocity such that the asteroid heads towards Earth
+                vx = -(((x - E_pos[0]))/7.78574E8); //+ (0.5*(x - E_pos[0]))/7.78574E11)*1e3 *(rand.nextDouble());
+                vy = -(((y - E_pos[1]))/7.78574E8); //+ (0.5*(x - E_pos[1]))/7.78574E11)*1e3 *(rand.nextDouble());
+                vz = 0.;//(((z - E_pos[2])) * rand.nextGaussian() + ((x - E_pos[2])));
+                a_vel[0] = vx;
+                a_vel[1] = vy;
+                a_vel[2] = vz;
+            } else {
+                vx = -2.7e4;
+                a_vel[0] = vx;
+            }
+            
+            // set the asteroid's radius such that it is a Gaussian about (50 +/- 33)m
+            double a_radius = rand.nextGaussian()*33. + 50.;
+            
+            // use Density * Volume to calculate mass; assume Density of asteroids = 5000 kg/m^3
+            // Volume of a sphere = 4/3 * PI * r^3
+            double a_mass = 5e3 *(4./3.) * Math.PI * a_radius * a_radius * a_radius;
+            Body ast = new Body(a_pos, a_vel, a_mass, a_radius, String.format("Asteroid%d", astNum));
+            
+            // add the asteroid to the system and increment the number of asteroids
+            addObject(ast);
+            astNum++; 
+            
+        }
+    }
+    
     
 }
